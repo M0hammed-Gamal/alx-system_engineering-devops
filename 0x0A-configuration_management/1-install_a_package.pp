d38b74da95e807b71cd85170e25235f48ecc7014
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-12-22 08:28:00 -0800

    config hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Ensure python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
