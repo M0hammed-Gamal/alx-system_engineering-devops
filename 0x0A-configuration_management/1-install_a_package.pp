12098630e0fca84d6e1b3526d48866228a54a645
M0hammed-Gamal <m_m20000075@yahoo.com>
2023-12-22 08:26:01 -0800

    config hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installing a package using puppet
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
