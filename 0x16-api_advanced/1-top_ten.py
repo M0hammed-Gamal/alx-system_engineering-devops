b5e0febb7b11306ab6f006b29d85b82753ed682a
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-02-13 18:40:29 +0000

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-
-"""
-query the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-
-from requests import get
-from sys import argv
+""" Exporting csv files"""
+import json
+import requests
+import sys
 
 
-def top_ten(subreddit: str) -> None:
-    """
-    function that does the heavy lifting for us
-    Args:
-        subreddit (str) -> The subreddit to query
-    Returns: The top 10 hots for that subreddit
-    """
-    headers = {
-        "User-Agent": "Marvel's Agents of Shield/21",
-        "X-Forwared-For": "Phil J. Coulson"
-    }
-
-    request_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-
-    try:
-        response = get(request_url, headers=headers,
-                       allow_redirects=False).json()
-        data = response['data']['children']
-        [print(post['data']['title']) for post in data[:10]]
-    except Exception:
-        print("None")
-
-
-if __name__ == "__main__":
-    (top_ten(argv[1]))
+def top_ten(subreddit):
+    """Read reddit API and return top 10 hotspots """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        list_titles = r.json()['data']['children']
+        for a in list_titles[:10]:
+            print(a['data']['title'])
+    else:
+        return(print("None"))
